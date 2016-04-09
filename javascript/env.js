5dcbec15b781af662814687a7088cd826d28bc4f
Kyly <kylyvass@gmail.com>
2016-04-09 12:58:12 -0700

    Added env.js

--- javascript/env.js
+++ javascript/env.js
@@ -4,8 +4,9 @@ var headers = require('./processHeader.js').parseHeader(process.env);
 var head =  'Content-Type: text/html\r\n\n' +
     '<!DOCTYPE html> <html> <head> <meta charset="UTF-8"><title>We code in our underpants</title>\n' +
     '</head><body>\n';
-var footer = '</body>\n</html>\n';
 
+out.write( head );
+var footer = '</body>\n</html>\n';
 function printTable(arr) {
     out.write('<table><tr><th>Name</th><th>Value</th></tr>');
     arr.forEach(function(el) {
@@ -13,11 +14,10 @@ function printTable(arr) {
         out.write('<td>' + el[0] + '</td><td>' + el[1] + '</td>');
         out.write('</tr>');
     });
-    out.write('</table>');
 
+    out.write('</table>');
 }
 
-out.write( head );
 out.write('<h1>Server</h1>');
 printTable(headers.server);
 out.write('<h1>Client</h1>');
