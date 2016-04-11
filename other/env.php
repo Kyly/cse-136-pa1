030cadff3ede94ecff4f62997a78378dbe7fc4ba
Abbas Dharamsey <adharams@ucsd.edu>
2016-04-10 19:50:17 -0700

    Finished PHP

--- other/env.php
+++ other/env.php
@@ -18,21 +18,31 @@ foreach($bo as $key_name => $key_value) {
 
 }
 
-Print ("<html>");
+
+Print ("<!DOCTYPE html><html>");
 Print ("<head>");
-Print ("<style>");
-Print ("table, th, td { border: 1px solid black; border-collapse: collapse; }");
-Print ("</style>");
+//Print ("<style>");
+//Print ("table, th, td { border: 1px solid black; border-collapse: collapse; }");
+//Print ("</style>");
 Print ("<title>We Code In Our Underpants</title>");
 Print ("<meta charset='UTF-8'");
 Print ("</head>");
 Print("<body style='background-color: white;'>");
 
-Print ("<h1>ENVIRONMENT VARIABLES</h1> <br><br>");
 
-Print ("<h2>BROWSER VARIABLES</h2>");
-Print ("<table>");
-Print ("<tr><th>ENVIRONMENT VARIABLE NAME</th><th>VALUE</th></tr>");
+
+Print ("<h1>Server</h1>");
+Print ("<table><tr><th>Name</th><th>Value</th></tr>");
+
+foreach($server as $key_name => $key_value) {
+    Print ("<tr><td>" . $key_name . "</td><td>" . $key_value . "</td></tr>");
+}
+
+Print ("</table>");
+Print ("<br></br>");
+
+Print ("<h1>Browser</h1>");
+Print ("<table><tr><th>Name</th><th>Value</th></tr>");
 
 foreach($browser as $key_name => $key_value) {
     Print ("<tr><td>" . $key_name . "</td><td>" . $key_value . "</td></tr>");
@@ -41,16 +51,6 @@ foreach($browser as $key_name => $key_value) {
 Print ("</table>");
 Print ("<br></br>");
 
-Print ("<h2>SERVER VARIABLES</h2>");
-Print ("<table>");
-Print ("<tr><th>ENVIRONMENT VARIABLE NAME</th><th>VALUE</th></tr>");
-
-foreach($server as $key_name => $key_value) {
-    Print ("<tr><td>" . $key_name . "</td><td>" . $key_value . "</td></tr>");
-}
-
-Print ("</table>");
-Print ("<br></br>");
 
 
 Print ("</body></html>");
