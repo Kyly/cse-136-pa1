035e88e03f90d6aa3f383bdc845ea934a3a64a17
Kevin <tpnxyjr@gmail.com>
2016-04-11 18:21:01 -0700

    html validation

--- cpp/hello-world.cpp
+++ cpp/hello-world.cpp
@@ -21,8 +21,8 @@ int main()
 
    cout << "Content-type: text/html" << endl << endl;
    cout << "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">" << endl;
-   cout << "<html>" << endl;
-   cout << "<head> <title>We code in our underpants</title> </head>" << endl;
+   cout << "<html xmlns=\"http://www.w3.org/1999/xhtml\">" << endl;
+   cout << "<head> <meta charset=\"utf-8\"/> <title>We code in our underpants</title> </head>" << endl;
    cout << "<body bgcolor="+color+">" << endl;
    cout << " <h1>Hello World! from C++ @" << str << "</h1>" << endl;
    cout << "   </body>" << endl;
